a00d89d2b137aa181b3bb3a9ea11a88883d68ded
Rohan Patil <166194152+codewithrohan5556@users.noreply.github.com>
2026-05-31 17:19:57 +0530

    Add files via upload

---
+++ 7.Chatbot_Streamlit_Deployment/Chatbot_frontend.py
@@ -0,0 +1,41 @@
+from Chatbot_backend import chatbot
+from langchain_core.messages import HumanMessage
+import streamlit as st
+
+# st.session_state -> dict -> dont erase content on display enter
+CONFIG = {'configurable':{'thread_id':'thread-1'}} 
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# loading conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+# {'role':'user','content':'hi'}
+# {'role':'assistant','content':'hello'}
+
+# with st.chat_message('user'):
+#     st.text('hi')
+
+# with st.chat_message('assistant'):
+#     st.text('how can i help you') 
+
+user_input = st.chat_input('Type here: ')
+
+if user_input:
+    # first add message to message history
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=CONFIG)
+    ai_message = response['messages'][-1].content
+    # first add message to message history
+    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message) 
+
+
