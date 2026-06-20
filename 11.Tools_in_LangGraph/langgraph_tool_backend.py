1f854d39f3f03920a0a72c4c690d3ab915ca6a0f
Rohan Patil <166194152+codewithrohan5556@users.noreply.github.com>
2026-06-20 11:19:07 +0530

    Add files via upload

---
+++ 11.Tools_in_LangGraph/langgraph_tool_backend.py
@@ -0,0 +1,121 @@
+# Backend
+
+from langgraph.graph import StateGraph,START,END
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage,HumanMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode,tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
+from dotenv import load_dotenv
+import sqlite3
+import requests
+
+load_dotenv()
+
+# ----------------------
+# 1.LLM
+# ----------------------
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+
+# ----------------------
+# 2.Tools
+# ----------------------
+
+search_tool = DuckDuckGoSearchRun(region='us-en')
+
+@tool
+def calculator(first_num:float,second_num:float,operation:str) -> dict:
+    """
+    Perform a basic arithmetic operation on two numbers.
+    Supported operaitons : add,sub,mul,div
+    """
+    try:
+        if operation=='add':
+            result = first_num+second_num
+        elif operation=='sub':
+            result = first_num-second_num    
+        elif operation=='mul':
+            result = first_num*second_num    
+        elif operation=='div':
+            if second_num == 0:
+                return {'error':'division by zero is not allowed'}
+            result = first_num/second_num 
+        else:
+            return {'error':f'Unsupported operation {operation}'}
+        return {'first_num':first_num,'second_num':second_num,'operation':operation,'result':result}
+    except Exception as e:
+        return {'error':str(e)}
+        
+
+@tool
+def get_stock_price(symbol:str)->dict:
+    """
+    Fetch latest stock price for a given symbol (eg. 'AAPL','TSLA')
+    using Alpha Vantage with API key in the URL.
+    """
+    url = f""
+    response = requests.get(url)
+    return response.json()
+
+tools = [get_stock_price,search_tool,get_stock_price]
+
+llm_with_tools = llm.bind_tools(tools)
+
+# ----------------------
+# 3.State
+# ----------------------
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage],add_messages]
+
+# ----------------------
+# 4.Node
+# ----------------------
+
+def chat_node(state:ChatState):
+    """
+    LLM node that may answer or request a tool call
+    """
+    messages = state['messages']
+    response = llm_with_tools.invoke(messages)
+    return {"messages":[response]}
+
+tool_node = ToolNode(tools)
+
+# ----------------------
+# 5.Checkpointer
+# ----------------------
+
+conn = sqlite3(database='chatbot.db',check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+
+# ----------------------
+# 6.Graph
+# ----------------------
+
+graph = StateGraph(ChatState)
+graph.add_node('chat_node',chat_node)
+graph.add_node('tools',tool_node)
+
+graph.add_edge(START,'chat_node')
+
+# If LLM asked for a tool go to ToolNode; else finish
+graph.add_conditional_edges('chat_node',tools_condition)
+graph.add_edge('tools','chat_node')
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+# ----------------------
+# 7.Helper
+# ----------------------
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpointer in checkpointer.list(None):
+        all_threads.add(checkpointer.config['configurable']['thread_id'])
+    return list(all_threads)
